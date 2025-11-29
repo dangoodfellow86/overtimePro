518522c989f3d80c86f9dd9864f170d1c14de9f4
Dan Goodfellow <daniel.goodfellow@sky.com>
2025-11-29 11:08:32 +0000

    fix: Update Firebase environment variable references for consistency

--- services/firebase.ts
+++ services/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.WEBAPP_FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  authDomain: process.env.WEBAPP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.WEB_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.WEB_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.WEB_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.WEB_FIREBASE_APP_ID,
+  measurementId: process.env.WEB_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
